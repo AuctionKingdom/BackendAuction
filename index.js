0795a86b3a1dc8691a15e6b4c9559ff8aae30d25
Akhilesh <akhilramkee123@gmail.com>
2020-05-14 17:06:03 +0530

    To ease the merge conflict

--- index.js
+++ index.js
@@ -3,14 +3,9 @@
 let express = require('express');
 let app = express();
 const port = process.env.PORT || 8080;
-let server = require('http').createServer(app);
-let io = require('socket.io')(server);
+let io = require('socket.io')(app.listen(port,()=>console.log(`Listening on port ${port}`)));
 const redis = require('./redisConnection.js');
 
 require('./services/connection.js')(io);
-
-console.log(`Running on http://localhost:8080`);
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports =  app;
