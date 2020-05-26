4d2bd98da6dbd6b8d10503f2c55c1563ec2d19cd
Akhilesh <akhilramkee123@gmail.com>
2020-05-26 14:00:08 +0530

    Module not found error uuid corrected

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uuidv1 = require('uuid/v1');
+const uuid = require('uuid')
 const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
@@ -35,7 +35,7 @@ userSchema.virtual('password')
     // create temporary variable called _password
     this._password = password
     // gernerate a timestamp
-    this.salt = uuidv1()
+    this.salt = uuid.v1()
     //encrypt password
     this.hashed_password = this.encryptPassword(password)
 })
